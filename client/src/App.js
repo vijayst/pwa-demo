da242fdc837565c8e625561e4ee9b0fb7f201c49
Vijay T <vijayst@gmail.com>
2021-09-07 16:16:06 +0530

    display hospitals in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [hospitals, setHospitals] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/hospitals').then((response) => {
+            setHospitals(response.data);
+        });
+    }, []);
+    return (
+        <div className="app">
+            <h1>Hospitals</h1>
+            {hospitals.map((hospital) => (
+                <div key={hospital.id} className="card">
+                    <h3>{hospital.name}</h3>
+                    <div>Specialities: {hospital.specialities}</div>
+                    <div>Doctors: {hospital.doctors}</div>
+                    <div>Surgeries: {hospital.surgeries}</div>
+                    <div>Bed count: {hospital.bedCount}</div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
