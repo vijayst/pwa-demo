d30487e59ca8c246ea40d17c789ca59c2eddc71a
Vijay T <vijayst@gmail.com>
2021-09-08 13:23:23 +0530

    update hospital api

--- server/index.js
+++ server/index.js
@@ -13,6 +13,23 @@ app.get('/hospitals', (req, res) => {
     res.json(hospitals);
 });
 
+app.post('/hospital/:id', (req, res) => {
+    const { id } = req.params;
+    const text = fs.readFileSync('hospitals.json');
+    const hospitals = JSON.parse(text);
+    const hospital = hospitals.find((h) => h.id === id);
+    if (hospital) {
+        hospital.specialities = req.body.specialities;
+        hospital.doctors = req.body.doctors;
+        hospital.surgeries = req.body.surgeries;
+        hospital.bedCount = req.body.bedCount;
+        const hospitalsText = JSON.stringify(hospitals);
+        fs.writeFileSync('hospitals.json', hospitalsText);
+        return res.send({ message: 'ok' });
+    }
+    res.status(500).send({ message: 'Incorrect hospital detail' });
+});
+
 app.listen(4000, () => {
     console.log('Listening on port 4000');
 });
