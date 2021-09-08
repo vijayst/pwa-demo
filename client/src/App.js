3862e95f9344cf2c2e7dd2bd5508a15d95862bb0
Vijay T <vijayst@gmail.com>
2021-09-08 13:58:21 +0530

    form ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 
 function App() {
     const [hospitals, setHospitals] = useState([]);
+    const [activeId, setActiveId] = useState();
+    const [hospitalData, setHospitalData] = useState({
+        specialities: 0,
+        doctors: 0,
+        surgeries: 0,
+        bedCount: 0,
+    });
+
     useEffect(() => {
         axios
             .get('http://localhost:4000/hospitals')
@@ -39,18 +47,106 @@ function App() {
                 };
             });
     }, []);
+
+    function handleEditClick(hospital) {
+        setActiveId(hospital.id);
+        setHospitalData(hospital);
+    }
+
+    function handleCancelClick() {
+        setActiveId(null);
+    }
+
+    function handleDataChange(e, key) {
+        setHospitalData({
+            ...hospitalData,
+            [key]: parseInt(e.target.value),
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitting');
+    }
+
     return (
         <div className="app">
             <h1>Hospitals</h1>
-            {hospitals.map((hospital) => (
-                <div key={hospital.id} className="card">
-                    <h3>{hospital.name}</h3>
-                    <div>Specialities: {hospital.specialities}</div>
-                    <div>Doctors: {hospital.doctors}</div>
-                    <div>Surgeries: {hospital.surgeries}</div>
-                    <div>Bed count: {hospital.bedCount}</div>
-                </div>
-            ))}
+            {hospitals.map((hospital) =>
+                activeId === hospital.id ? (
+                    <form
+                        className="form"
+                        key={hospital.id}
+                        onSubmit={handleSubmit}
+                    >
+                        <h3>{hospital.name}</h3>
+                        <div className="form__control">
+                            <label>Specialities</label>
+                            <input
+                                type="number"
+                                value={hospitalData.specialities}
+                                onChange={(e) =>
+                                    handleDataChange(e, 'specialities')
+                                }
+                            />
+                        </div>
+                        <div className="form__control">
+                            <label>Doctors</label>
+                            <input
+                                type="number"
+                                value={hospitalData.doctors}
+                                onChange={(e) => handleDataChange(e, 'doctors')}
+                            />
+                        </div>
+                        <div className="form__control">
+                            <label>Surgeries</label>
+                            <input
+                                type="number"
+                                value={hospitalData.surgeries}
+                                onChange={(e) =>
+                                    handleDataChange(e, 'surgeries')
+                                }
+                            />
+                        </div>
+                        <div className="form__control">
+                            <label>Bed count</label>
+                            <input
+                                type="number"
+                                value={hospitalData.bedCount}
+                                onChange={(e) =>
+                                    handleDataChange(e, 'bedCount')
+                                }
+                            />
+                        </div>
+                        <div className="buttons">
+                            <button
+                                type="button"
+                                className="button"
+                                onClick={handleCancelClick}
+                            >
+                                Cancel
+                            </button>
+                            <button className="button">Update</button>
+                        </div>
+                    </form>
+                ) : (
+                    <div key={hospital.id} className="card">
+                        <h3>{hospital.name}</h3>
+                        <div>Specialities: {hospital.specialities}</div>
+                        <div>Doctors: {hospital.doctors}</div>
+                        <div>Surgeries: {hospital.surgeries}</div>
+                        <div>Bed count: {hospital.bedCount}</div>
+                        <div className="buttons">
+                            <button
+                                className="button"
+                                onClick={() => handleEditClick(hospital)}
+                            >
+                                Edit
+                            </button>
+                        </div>
+                    </div>
+                )
+            )}
         </div>
     );
 }
